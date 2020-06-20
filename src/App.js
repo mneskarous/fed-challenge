b7b67e1d0e52237cdb60d31b9f136618025c0558
Michael Eskarous <mneskarous@gmail.com>
2020-06-20 11:45:09 -0700

    Created Product Nav component and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+
+import ProductNav from './components/product-nav/product-nav.component'
+
+// import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="productnav-container">
+        <ProductNav/>
+      </div>
     </div>
   );
 }
---
+++ src/components/product-nav/product-nav.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './product-nav.styles.scss'
+
+const ProductNav = () => (
+  <div className="product-nav-links-container">
+    <div className="product-nav-link-container">
+      <button className="product-nav-link-btn">Blog</button>
+    </div>
+    <div className="product-nav-link-container">
+      <button className="product-nav-link-btn">Nourish</button>
+    </div>
+    <div className="product-nav-link-container">
+      <button className="product-nav-link-btn">Shop</button>
+    </div>
+  </div>
+);
+
+export default ProductNav;
