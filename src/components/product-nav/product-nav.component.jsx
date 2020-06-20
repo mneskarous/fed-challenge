fa5b52efa01c202ee7ee7cd05e68f33fcee918bb
Michael Eskarous <mneskarous@gmail.com>
2020-06-20 13:08:34 -0700

    Modifed class name and modified structure of sass file

--- src/components/product-nav/product-nav.component.jsx
+++ src/components/product-nav/product-nav.component.jsx
@@ -4,13 +4,13 @@ import './product-nav.styles.scss'
 
 const ProductNav = () => (
   <div className="product-nav-links-container">
-    <div className="product-nav-link-container">
+    <div className="product-nav-link-btn-container">
       <button className="product-nav-link-btn">Blog</button>
     </div>
-    <div className="product-nav-link-container">
+    <div className="product-nav-link-btn-container">
       <button className="product-nav-link-btn">Nourish</button>
     </div>
-    <div className="product-nav-link-container">
+    <div className="product-nav-link-btn-container">
       <button className="product-nav-link-btn">Shop</button>
     </div>
   </div>
