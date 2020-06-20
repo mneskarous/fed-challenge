fc8e7de8ba73d222eec8c1f83266e6184cdba323
Michael Eskarous <mneskarous@gmail.com>
2020-06-20 13:05:23 -0700

    Created Main Nav component and styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import ProductNav from './components/product-nav/product-nav.component'
+import MainNav from './components/main-nav/main-nav.component';
 
-// import './App.css';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <div className="productnav-container">
-        <ProductNav/>
+      <div className="product-nav-container">
+        <ProductNav />
+      </div>
+      <div className="main-nav-container">
+        <MainNav />
       </div>
     </div>
   );
---
+++ src/components/main-nav/main-nav.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './main-nav.styles.scss';
+
+const MainNav = () => (
+  <div className="main-nav-items-container">
+    <div className="main-nav-logo-container">
+      <img src="./images/ifit-coach-logo.svg" alt="iFit Coach Logo"/>
+    </div>
+    <div className="main-nav-links-container">
+      <button className="main-nav-link">Exercise</button>
+      <button className="main-nav-link">Nutrition</button>
+      <button className="main-nav-link">Activity</button>
+      <button className="main-nav-link">Sleep</button>
+    </div>
+    <div className="main-nav-links-btn-container">
+    <button className="main-nav-link-btn">Sign Up</button>
+    </div>
+  </div>
+);
+
+export default MainNav
