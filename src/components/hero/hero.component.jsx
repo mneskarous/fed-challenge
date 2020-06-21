94d9d87496a15cdf74bcc7006ff93a773ff0dea4
Michael Eskarous <mneskarous@gmail.com>
2020-06-21 00:57:13 -0700

    Created animated fade-in effect for hero text

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -5,7 +5,7 @@ import './hero.styles.scss';
 const Hero = () => (
   <div className="hero-container">
     <div className="hero-text-container">
-      <p className="hero-text">The best personal training, <br/>right in your own home</p>
+      <p className="hero-text animate-flicker">The best personal training, <br/>right in your own home</p>
     </div>
     <div className="hero-btn-container">
       <button className="hero-btn">Join iFit Coach</button>
