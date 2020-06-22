be20717b992a4529711fd9d6653b9b2139e77c28
Michael Eskarous <mneskarous@gmail.com>
2020-06-21 23:00:08 -0700

    Cleaned code for MediaSlider component

--- src/components/media-slider/media-slider.component.jsx
+++ src/components/media-slider/media-slider.component.jsx
@@ -26,38 +26,42 @@ const responsive = {
   }
 };
 
-const MediaSlider = () => (
-  <div className="media-card-container">
-    <Carousel 
-      responsive={responsive}
-      swipeable={true}
-    >
-      <div className="media-card">
-        <img src="./images/gear-junkie-logo.svg" alt="Gear Junkie logo"/>
-        <p className="media-card-text">"You focus on putting in the work, and the technology handles the rest."</p>
-      </div>
-      <div className="media-card">
-        {/* eslint-disable-next-line */}
-        <img src="./images/gear-junkie-logo.svg" alt="Gear Junkie logo"/>
-        <p className="media-card-text">"You focus on putting in the work, and the technology handles the rest."</p>
-      </div>
-      <div className="media-card">
-        {/* eslint-disable-next-line */}
-        <img src="./images/wired-logo.svg" alt="Gear Junkie logo" alt="Wired logo"/>
-        <p className="media-card-text">"Literally transports you from home to wherever you choose to run."</p>
-      </div>
-      <div className="media-card">
-        {/* eslint-disable-next-line */}
-        <img src="./images/mashable-logo.svg" alt="Gear Junkie logo" alt="Mashable logo"/>
-        <p className="media-card-text">"Breathes new life into a tired, old running routine."</p>
-      </div>
-      <div className="media-card">
-        {/* eslint-disable-next-line */}
-        <img src="./images/mashable-logo.svg" alt="Gear Junkie logo" alt="Mashable logo"/>
-        <p className="media-card-text">"Breathes new life into a tired, old running routine."</p>
-      </div>
-    </Carousel>
-  </div>
-);
+function MediaSlider() {
+
+  return (
+    <div className="media-card-container">
+      <Carousel 
+        responsive={responsive}
+        swipeable={true}
+      >
+        <div className="media-card">
+          <img src="./images/gear-junkie-logo.svg" alt="Gear Junkie logo"/>
+          <p className="media-card-text">"You focus on putting in the work, and the technology handles the rest."</p>
+        </div>
+        <div className="media-card">
+          {/* eslint-disable-next-line */}
+          <img src="./images/gear-junkie-logo.svg" alt="Gear Junkie logo"/>
+          <p className="media-card-text">"You focus on putting in the work, and the technology handles the rest."</p>
+        </div>
+        <div className="media-card">
+          {/* eslint-disable-next-line */}
+          <img src="./images/wired-logo.svg" alt="Gear Junkie logo" alt="Wired logo"/>
+          <p className="media-card-text">"Literally transports you from home to wherever you choose to run."</p>
+        </div>
+        <div className="media-card">
+          {/* eslint-disable-next-line */}
+          <img src="./images/mashable-logo.svg" alt="Gear Junkie logo" alt="Mashable logo"/>
+          <p className="media-card-text">"Breathes new life into a tired, old running routine."</p>
+        </div>
+        <div className="media-card">
+          {/* eslint-disable-next-line */}
+          <img src="./images/mashable-logo.svg" alt="Gear Junkie logo" alt="Mashable logo"/>
+          <p className="media-card-text">"Breathes new life into a tired, old running routine."</p>
+        </div>
+      </Carousel>
+    </div>
+  );
+
+}
 
 export default MediaSlider;
