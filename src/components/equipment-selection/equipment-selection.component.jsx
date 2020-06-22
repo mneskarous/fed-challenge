3fc2d5abb023509f95873f5863ef0c1b356751dc
Michael Eskarous <mneskarous@gmail.com>
2020-06-21 22:45:10 -0700

    Added box shadow animation on- and off-hover and fade-in animation when coming into view for Equipment Cards

--- src/components/equipment-selection/equipment-selection.component.jsx
+++ src/components/equipment-selection/equipment-selection.component.jsx
@@ -2,29 +2,74 @@ import React from 'react';
 
 import './equipment-selection.styles.scss'
 
-const EquipmentSelection = () => (
-  <div className="equipment-selection-container">
+function EquipmentSelection() {
+  const [isVisible, setVisible] = React.useState(true);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+    return () => observer.unobserve(domRef.current);
+  }, []);
+
+  return (
+    <div className="equipment-selection-container">
     <p className="equipment-selection-title">Interested in our exciting iFit-enabled equipment?</p>
     <div className="equipment-cards">
-      <div className="equipment-card">
+      <div 
+        className={`equipment-card fade-in-section ${isVisible ? 'is-visible' : ''}`}
+        ref={domRef}
+      >
         <img className="equipment-image" src="./images/treadmills.png" alt="Treadmills"/>
         <div className="equipment-title">Treadmills</div>
       </div>
-      <div className="equipment-card">
+      <div 
+        className={`equipment-card fade-in-section ${isVisible ? 'is-visible' : ''}`}
+        ref={domRef}
+      >
       <img className="equipment-image" src="./images/bikes.png" alt="Bikes"/>
         <div className="equipment-title">Bikes</div>
       </div>
-      <div className="equipment-card">
+      <div 
+        className={`equipment-card fade-in-section ${isVisible ? 'is-visible' : ''}`}
+        ref={domRef}
+      >
       <img className="equipment-image" src="./images/ellipticals.png" alt="Ellipticals"/>
         <div className="equipment-title">Ellipticals</div>
       </div>
-      <div className="equipment-card">
+      <div 
+        className={`equipment-card fade-in-section ${isVisible ? 'is-visible' : ''}`}
+        ref={domRef}
+      >
         <img className="equipment-image" src="./images/strength.png" alt="Strength Training Equipment"/>
         <div className="equipment-title">Strength</div>
       </div>
     </div>
-    
   </div>
-);
+  )
+
+}
 
 export default EquipmentSelection;
+
+
+function FadeInSection(props) {
+  const [isVisible, setVisible] = React.useState(true);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+    return () => observer.unobserve(domRef.current);
+  }, []);
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
