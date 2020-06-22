fddf1feb95567f20905733c8c8c1b7239c4ba1fd
Michael Eskarous <mneskarous@gmail.com>
2020-06-21 23:01:03 -0700

    Cleaned code for ProductNav component

--- src/components/product-nav/product-nav.component.jsx
+++ src/components/product-nav/product-nav.component.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 import './product-nav.styles.scss'
 
-const ProductNav = () => (
-  <div className="product-nav-container">
-    <div className="product-nav-link-container">
-      <button className="product-nav-link">Blog</button>
+function ProductNav() {
+
+  return(
+    <div className="product-nav-container">
+      <div className="product-nav-link-container">
+        <button className="product-nav-link">Blog</button>
+      </div>
+      <div className="product-nav-link-container">
+        <button className="product-nav-link">Nourish</button>
+      </div>
+      <div className="product-nav-link-container">
+        <button className="product-nav-link">Shop</button>
+      </div>
     </div>
-    <div className="product-nav-link-container">
-      <button className="product-nav-link">Nourish</button>
-    </div>
-    <div className="product-nav-link-container">
-      <button className="product-nav-link">Shop</button>
-    </div>
-  </div>
-);
+  );
+
+}
 
 export default ProductNav;
