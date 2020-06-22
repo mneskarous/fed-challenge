08ea72052b254d0f3fdeec36b3035b0495ff0c04
Michael Eskarous <mneskarous@gmail.com>
2020-06-21 22:58:21 -0700

    Cleaned code for MainNav component

--- src/components/main-nav/main-nav.component.jsx
+++ src/components/main-nav/main-nav.component.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 
 import './main-nav.styles.scss';
 
-const MainNav = () => (
-  <div className="main-nav-container">
-    <div className="main-nav-logo-container">
-      <img src="./images/ifit-coach-logo.svg" alt="iFit Coach Logo"/>
-    </div>
-    <div className="main-nav-links-container">
-      <button className="main-nav-link">Exercise</button>
-      <button className="main-nav-link">Nutrition</button>
-      <button className="main-nav-link">Activity</button>
-      <button className="main-nav-link">Sleep</button>
-    </div>
-    <div className="main-nav-links-btn-container">
-    <button className="main-nav-link-btn">Sign Up</button>
-    </div>
-  </div>
-);
+function MainNav() {
 
-export default MainNav
+  return (
+    <div className="main-nav-container">
+      <div className="main-nav-logo-container">
+        <img src="./images/ifit-coach-logo.svg" alt="iFit Coach Logo"/>
+      </div>
+      <div className="main-nav-links-container">
+        <button className="main-nav-link">Exercise</button>
+        <button className="main-nav-link">Nutrition</button>
+        <button className="main-nav-link">Activity</button>
+        <button className="main-nav-link">Sleep</button>
+      </div>
+      <div className="main-nav-links-btn-container">
+      <button className="main-nav-link-btn">Sign Up</button>
+      </div>
+    </div>
+  );
+
+}
+
+export default MainNav;
