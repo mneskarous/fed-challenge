098d863d704c0cf51d4f64cd249e29a242a1fad8
Michael Eskarous <mneskarous@gmail.com>
2020-06-21 22:56:40 -0700

    Cleaned code for Hero component

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 import './hero.styles.scss';
 
-const Hero = () => (
-  <div className="hero-container">
-    <div className="hero-text-container">
-      <p className="hero-text animate-flicker">The best personal training, <br/>right in your own home</p>
+function Hero() {
+
+  return (
+    <div className="hero-container">
+      <div className="hero-text-container">
+        <p className="hero-text animate-flicker">The best personal training, <br/>right in your own home</p>
+      </div>
+      <div className="hero-btn-container">
+        <button className="hero-btn">Join iFit Coach</button>
+      </div>
     </div>
-    <div className="hero-btn-container">
-      <button className="hero-btn">Join iFit Coach</button>
-    </div>
-  </div>
-);
+  );
+
+}
 
 export default Hero;
