70cdfcb7ef3d92e502a4f1b77d1aa13687b4ac43
Michael Eskarous <mneskarous@gmail.com>
2020-06-21 23:36:32 -0700

    Refactored to have fade-in animation effect on parent element

--- src/components/equipment-selection/equipment-selection.component.jsx
+++ src/components/equipment-selection/equipment-selection.component.jsx
@@ -16,32 +16,23 @@ function EquipmentSelection() {
   return (
     <div className="equipment-selection-container">
       <p className="equipment-selection-title">Interested in our exciting iFit-enabled equipment?</p>
-      <div className="equipment-cards">
-        <div 
-          className={`equipment-card fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      <div 
+          className={`equipment-cards fade-in-section ${isVisible ? 'is-visible' : ''}`}
           ref={domRef}
         >
+        <div className="equipment-card">
           <img className="equipment-image" src="./images/treadmills.png" alt="Treadmills"/>
           <div className="equipment-title">Treadmills</div>
         </div>
-        <div 
-          className={`equipment-card fade-in-section ${isVisible ? 'is-visible' : ''}`}
-          ref={domRef}
-        >
+        <div className="equipment-card">
           <img className="equipment-image" src="./images/bikes.png" alt="Bikes"/>
           <div className="equipment-title">Bikes</div>
         </div>
-        <div 
-          className={`equipment-card fade-in-section ${isVisible ? 'is-visible' : ''}`}
-          ref={domRef}
-        >
+        <div className="equipment-card">
           <img className="equipment-image" src="./images/ellipticals.png" alt="Ellipticals"/>
           <div className="equipment-title">Ellipticals</div>
         </div>
-        <div 
-          className={`equipment-card fade-in-section ${isVisible ? 'is-visible' : ''}`}
-          ref={domRef}
-        >
+        <div className="equipment-card">
           <img className="equipment-image" src="./images/strength.png" alt="Strength Training Equipment"/>
           <div className="equipment-title">Strength</div>
         </div>
